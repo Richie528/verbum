9ba460c9753a2cf32dacd52be10b6edb025dce42
Reechee <yangrichie528@gmail.com>
2024-05-17 20:48:57 +1000

    Input stuff and wordlist stuff

--- script.js
+++ script.js
@@ -2,14 +2,36 @@
 let wordlists = [["porto", "habet"], ["canis", "cibus"], ["laborat", "caudex"]];
 
 /* VARIABLES */
+let cycles = 0;
 let wordlist = [];
-let stagesSelected = [];
+let stagesSelected = []; for (let i = 0; i < 40; i++) {stagesSelected.push(false);}
 let currentWordlist = [];
 let currentWords = [];
+let previousInput = "";
 let words = [];
+let normalWordState = ["word-small", "word-big", "word-small", "word-gone"];
+let animatedWordState = ["word-gone", "word-small", "word-big", "word-small"];
 
-/* UPDATING WORD LIST */
-function updateWordList() {
+/* ELEMENTS */
+let stageButtons = document.querySelectorAll(".stage-button");
+words.push(document.getElementById("p"));
+words.push(document.getElementById("c"));
+words.push(document.getElementById("n"));
+words.push(document.getElementById("m"));
+previousInputElement = document.getElementById("p-input");
+currentInputElement = document.getElementById("c-input");
+
+/* DISPLAY WORDS */
+function displayWords() {
+  for (let i = 0; i < 4; i++) {
+    words[i].querySelector(".prompt").textContent = currentWords[i];
+  }
+  previousInputElement.value = previousInput;
+}
+
+/* INITIALISE CURRENT WORDLIST AND WORLISTS */
+function initialise() {
+  cycles = 0;
   wordlist = [];
   for (let i = 0; i < 40; i++) {
     if (stagesSelected[i]) {
@@ -18,11 +40,37 @@ function updateWordList() {
       }
     }
   }
+  currentWords = [];
+  currentWordlist = [...wordlist];
+  for (let i = 0; i < 4; i++) {
+    let random = Math.floor(Math.random() * currentWordlist.length);
+    currentWords.push(currentWordlist[random]);
+    currentWordlist.splice(random, 1);
+
+    if (currentWordlist.length === 0) {
+      currentWordlist = [...wordlist];
+      cycles += 1;
+    }
+  }
+  displayWords();
+}
+
+/* GET NEW WORD */
+function getNewWord() {
+  previousInput = currentInputElement.value;
+  currentInputElement.value = "";
+  currentWords.splice(0, 1);
+  let random = Math.floor(Math.random() * currentWordlist.length);
+  currentWords.push(currentWordlist[random]);
+  currentWordlist.splice(random, 1);
+
+  if (currentWordlist.length === 0) {
+    currentWordlist = [...wordlist];
+    cycles += 1;
+  }
 }
 
 /* STAGE SELECTION */
-for (let i = 0; i < 40; i++) {stagesSelected.push(false);}
-let stageButtons = document.querySelectorAll(".stage-button");
 for (let i = 0; i < 40; i++) {
   stageButtons[i].onclick = function(event) {
     if (event.shiftKey) {
@@ -37,22 +85,18 @@ for (let i = 0; i < 40; i++) {
       else {stageButtons[i].style.backgroundColor = "#11111b";}
       stagesSelected[i] = !stagesSelected[i];
     }
-    updateWordList();
+    initialise();
   }
 }
 
 /* SHUFFLE WORDS */
-let normalWordState = ["word-small", "word-big", "word-small", "word-gone"];
-let animatedWordState = ["word-gone", "word-small", "word-big", "word-small"];
-words.push(document.getElementById("p"));
-words.push(document.getElementById("c"));
-words.push(document.getElementById("n"));
-words.push(document.getElementById("m"));
 function shuffleWords() {
   for (let i = 0; i < 4; i++) {words[i].classList.remove(normalWordState[i]);}
   for (let i = 0; i < 4; i++) {words[i].classList.add(animatedWordState[i]);}
   setTimeout(function() {
     for (let i = 0; i < 4; i++) {words[i].classList.add("dont-animate");}
+    getNewWord();
+    displayWords();
     for (let i = 0; i < 4; i++) {words[i].classList.add(normalWordState[i]);}
     for (let i = 0; i < 4; i++) {words[i].classList.remove(animatedWordState[i]);}
     for (let i = 0; i < 4; i++) {words[i].offsetHeight}
@@ -65,6 +109,5 @@ document.body.addEventListener('keydown', function (event) {
   switch (key) {
     case "Enter":
       shuffleWords();
-      console.log("ANIMATE!!");
   }
 });
